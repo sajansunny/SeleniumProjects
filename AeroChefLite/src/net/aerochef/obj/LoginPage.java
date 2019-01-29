a7d12a1dad800b5401daa932514106ff801c80de
admin <admin@DESKTOP-IBO3EG0>
2019-01-29 06:42:49 +0530

    First Commit

---
+++ AeroChefLite/src/net/aerochef/obj/LoginPage.java
@@ -0,0 +1,10 @@
+package net.aerochef.obj;
+
+import org.openqa.selenium.By;
+
+public class LoginPage {
+	public By UserName = By.id("txtUserName");
+	public By Password = By.id("txtPwd");
+	public By Login = By.name("Button1");
+
+}
---
+++ AeroChefLite/src/net/aerochef/test/LoginTest.java
@@ -0,0 +1,19 @@
+package net.aerochef.test;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class LoginTest {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
